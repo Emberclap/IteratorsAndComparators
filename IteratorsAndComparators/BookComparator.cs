bc5b5a22b0b062106836a307bcaebc804c7ff68f
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-10-12 13:46:09 +0300

    Add project files.

---
+++ IteratorsAndComparators/BookComparator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class BookComparator : IComparer<Book>
+    {
+        public  int Compare(Book a, Book b)
+        {
+            int result = a.Title.CompareTo(b.Title);
+            if (result == 0)
+            {
+               result = a.Year.CompareTo(b.Year);
+            }
+            return result;
+        }
+    }
+}
